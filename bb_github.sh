c6cb535539a7124b6414633b46f784bc5692c66b
zhou <350360762@qq.com>
2021-09-02 16:45:19 +0800

    .

--- bb_github.sh
+++ bb_github.sh
@@ -1,6 +1,11 @@
+mv _site_github/_site/.git ./_site_github/
+
 jekyll build --config _config.yml,_config_github_prod.yml
 
-cd _site_github
+mv ./_site_github/.git _site_github/_site/ 
+
+cd _site_github/_site
+
 git add .
 git commit -m "."
 git push -u origin master
